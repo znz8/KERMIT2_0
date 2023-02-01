a6a49cb374fcd982dac3569f152b87560b5ff05e
LeonardRanaldi <57565268+LeonardRanaldi@users.noreply.github.com>
2023-02-01 17:32:31 +0100

    Update dte.py

--- kerMIT/kerMIT/structenc/dte.py
+++ kerMIT/kerMIT/structenc/dte.py
@@ -162,7 +162,7 @@ class DTE(DSE):
             baseChildrenList = [[]]
             for child in tree.children:
                 newBaseChildrenList = []
-                c_setOfTrees, c_setOfSubtrees = DT.subtrees(child)
+                c_setOfTrees, c_setOfSubtrees = DTE.subtrees(child)
                 setOfSubTrees = setOfSubTrees + c_setOfSubtrees
                 for treeSubChild in c_setOfTrees:
                    for treeSub in baseChildrenList:
